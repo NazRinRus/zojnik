30688562b93176fe8607136230a5df4d95f3c34f
Dmitriy <aegis.ko20@mail.ru>
2024-06-06 10:58:54 +0300

    initial structure

---
+++ src/routes/Router.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { routes } from './routes.data';
+
+const Router = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				{routes.map(route => {
+					return (
+						<Route
+							key={route.path}
+							element={<route.component />}
+							path={route.path}
+						/>
+					);
+				})}
+				<Route element={<div>Not Found</div>} path='*' />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default Router;
