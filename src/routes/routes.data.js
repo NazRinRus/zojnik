ac232b40d91d4acbc805834dbfba0f1fd10d5db6
Danila <emelchenko1997@yandex.ru>
2024-06-10 22:50:47 +0300

    Кнопки для Profile

--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -2,6 +2,8 @@ import Constructor from '../components/screens/constructor/Constructor';
 import Home from '../components/screens/home/Home';
 import Welcome from '../components/screens/welcome/Welcome';
 
+import Profile from '../components/screens/profile/Profile';
+
 export const routes = [
 	{
 		path: '/',
@@ -16,6 +18,11 @@ export const routes = [
 	{
 		path: '/constructor',
 		component: Constructor,
+		isAuth: false,
+	},
+  {
+		path: '/profile',
+		component: Profile,
 		isAuth: false,
 	},
 ];
