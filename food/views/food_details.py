fdba18a1f5911547acee281cc27f31dfc87b54e6
NazRinRus <NazRinRus@gmail.com>
2024-06-18 21:33:56 +0300

    Сделано: 1) Модели Блюдо, с соответствующим ему спискам, Тарелка; 2) Админпанель; 3) Переопределил user; 4) API авторизации и регистрации; 5) API словарей и пагинация; 6) API блюда; 7) Комментарии и рейтинги; 8) Тэги и Антитэги

--- food/views/food_details.py
+++ food/views/food_details.py
@@ -1,13 +1,13 @@
 from drf_spectacular.utils import extend_schema_view, extend_schema
-from food.models.food_details import Food
+from food.models.food_details import Food, FoodCategory
 from food.serializers import food_details
 from common.views.mixins import LCRUViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 @extend_schema_view(
-    list=extend_schema(summary='Список блюд (Тэги и антитэги указываются в параметрах запроса и разделяются символом %,'
-                               ' например: /api/food?tags=Мясо%Гарнир&antitags=Семена кунжута%Куркума%Паприка)', tags=['Блюда']),
+    list=extend_schema(summary='Список блюд (Тэги, антитэги и категория указываются в параметрах запроса и разделяются символом %,'
+                               ' например: api/food?tags=Мясо%Гарнир&antitags=Семена кунжута%Куркума%Паприка&category=Белковое блюдо)', tags=['Блюда']),
     retrieve=extend_schema(summary='Блюдо детально', tags=['Блюда']),
     create=extend_schema(summary='Создать блюдо', tags=['Блюда']),
     update=extend_schema(summary='Изменить блюдо', tags=['Блюда']),
@@ -29,23 +29,33 @@ class FoodView(LCRUViewSet):
     http_method_names = ('get', 'post', 'patch')
 
     def get_queryset(self):
+        category = []
+        if 'category' in self.request.GET:
+            category = self.request.GET['category'].split('%')
+        else:
+            category_qs = FoodCategory.objects.all()
+            for cat_name in category_qs:
+                category.append(cat_name.name)
         if ('tags' in self.request.GET) and ('antitags' in self.request.GET):
             tags = self.request.GET['tags'].split('%')
             antitags = self.request.GET['antitags'].split('%')
             qs = Food.objects.prefetch_related(
                 'tag',
                 'antitag',
-            ).filter(tag__name__in=tags).exclude(antitag__name__in=antitags)
+                'category',
+            ).filter(tag__name__in=tags, category__name__in=category).exclude(antitag__name__in=antitags)
         elif 'tags' in self.request.GET:
             tags = self.request.GET['tags'].split('%')
             qs = Food.objects.prefetch_related(
                 'tag',
-            ).filter(tag__name__in=tags)
+                'category',
+            ).filter(tag__name__in=tags, category__name__in=category)
         elif 'antitags' in self.request.GET:
             antitags = self.request.GET['antitags'].split('%')
             qs = Food.objects.prefetch_related(
                 'antitag',
-            ).exclude(antitag__name__in=antitags)
+                'category',
+            ).filter(category__name__in=category).exclude(antitag__name__in=antitags)
         else:
             qs = Food.objects.all()
         return qs
